ac59d09496507641167fe4ae86718688d39cdd76
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2024-12-08 16:36:37 +0900

    fix: MySQLのユーザー設定をデフォルトに戻した

--- home/isucon/webapp/sql/0-init.sql
+++ home/isucon/webapp/sql/0-init.sql
@@ -4,8 +4,5 @@ SET CHARACTER_SET_CONNECTION = utf8mb4;
 CREATE DATABASE IF NOT EXISTS isuride DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 DROP USER IF EXISTS 'isucon'@'%';
--- CREATE USER IF NOT EXISTS 'isucon'@'%' IDENTIFIED BY 'isucon';
--- GRANT ALL ON isuride.* TO 'isucon'@'%';
-
-CREATE USER IF NOT EXISTS 'isucon'@'192.168.%' IDENTIFIED BY 'isucon';
-GRANT ALL PRIVILEGES ON *.* TO 'isucon'@'192.168.%';
+CREATE USER IF NOT EXISTS 'isucon'@'%' IDENTIFIED BY 'isucon';
+GRANT ALL ON isuride.* TO 'isucon'@'%';
